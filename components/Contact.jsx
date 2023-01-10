7e8c1f25424f6cbfc4abd2f5c331ce69b58713c5
FerraroSoftware <software.ferraro@gmail.com>
2023-01-10 18:50:27 -0500

    hover

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
         <div className="max-w-5xl mx-auto mt-12 sm:mt-16">
           <div className="grid grid-cols-1 gap-6 px-8 text-center md:px-0 md:grid-cols-3">
             <div className="overflow-hidden bg-white rounded-xl">
-              <div className="p-6">
+              <div className="p-6 ">
                 <svg
                   className="flex-shrink-0 w-10 h-10 mx-auto text-gray-400"
                   xmlns="http://www.w3.org/2000/svg"
@@ -32,12 +32,14 @@ export default function Contact() {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <Link
-                  href="tel:7273176717"
-                  className="mt-6 text-lg font-medium text-gray-900"
-                >
-                  727-317-6717
-                </Link>
+                <div className="mt-6">
+                  <Link
+                    href="tel:7273176717"
+                    className=" text-lg font-medium text-gray-900 hover:text-red-600"
+                  >
+                    727-317-6717
+                  </Link>
+                </div>
               </div>
             </div>
 
