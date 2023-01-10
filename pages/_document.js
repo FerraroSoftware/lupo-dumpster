7dda8e58926e0a8f9aa8eee70dbaa4da6227d0cb
FerraroSoftware <software.ferraro@gmail.com>
2023-01-10 10:33:56 -0500

    google analitcs

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html className="h-full antialiased scroll-smooth" lang="en">
+      <Head>
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-V0GLH72P5Z');
+        `,
+          }}
+        />
+        <script dangerouslySetInnerHTML={{ __html: modeScript }} />
+      </Head>
+      <body className="bg-zinc-200 dark:bg-zinc-900">
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-V0GLH72P5Z"
+        ></Script>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
