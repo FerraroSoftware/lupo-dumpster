94c1845bb210c504b20dc490c50006ae32d8e358
FerraroSoftware <software.ferraro@gmail.com>
2024-09-11 12:43:00 -0400

    fb

--- pages/thank-you.jsx
+++ pages/thank-you.jsx
@@ -37,6 +37,26 @@ export default function thankyou() {
         }}
       />
 
+      {/* capture lead */}
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '892278365833500');
+            fbq('track', 'Lead');
+        `,
+        }}
+      />
+
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-white">
         <div className="sm:mx-auto sm:w-full sm:max-w-lg">
           <Image
