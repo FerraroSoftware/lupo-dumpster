6d87565d44356773d5fbd3939bbcdd86122eb564
FerraroSoftware <software.ferraro@gmail.com>
2025-03-02 20:41:01 -0500

    fi

--- components/ContactHeroTwo.jsx
+++ components/ContactHeroTwo.jsx
@@ -172,7 +172,7 @@ export default function HeroSection() {
             <div className="grid grid-cols-1 gap-4 text-white my-6">
               <div className="flex items-center gap-2">
                 <Truck className="h-5 w-5 text-red-500" />
-                <LucideHeading2>10, 15, 20 Yard Dumpsters</LucideHeading2>
+                <h2>10, 15, 20 Yard Dumpsters</h2>
               </div>
               <div className="flex items-center gap-2">
                 <MapPin className="h-5 w-5 text-red-500" />
