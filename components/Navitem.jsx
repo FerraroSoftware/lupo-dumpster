bd2b9496c7f1ff36309eed5d0bb9955c57c36630
FerraroSoftware <software.ferraro@gmail.com>
2025-03-10 13:05:25 -0400

    fix

--- components/Navitem.jsx
+++ components/Navitem.jsx
@@ -42,7 +42,7 @@ const services = {
     { title: "Mattress Disposal", href: "/services/mattress-disposal" },
   ],
   "Trash & Waste Services": [
-    { title: "Junk Removal", href: "/just-removal" },
+    { title: "Junk Removal", href: "/junk-removal" },
     { title: "Yard Waste Removal", href: "/services/yard-waste-removal" },
     { title: "Trash Pick-Up", href: "/services/trash-pick-up" },
     { title: "Trash Removal", href: "/services/trash-removal" },
