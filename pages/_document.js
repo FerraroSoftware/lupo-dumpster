a37a2120cc7922f0325129db2c8d2ab6d9bc2058
FerraroSoftware <software.ferraro@gmail.com>
2025-10-02 08:27:29 -0400

    tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,42 @@ export default function Document() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-V0GLH72P5Z');
+            gtag('config', 'AW-17610711210');
         `,
+          }}
+        />
+        {/* Call Tracking */}
+        <Script
+          id="google-ad-call-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          gtag('config', 'AW-17610711210/DOx5CLaEvqUbEKrBuM1B', {
+            'phone_conversion_number': '(727) 317-6717'
+          });
+      `,
+          }}
+        />
+        {/* Button Clicked Call Tracking */}
+        <Script
+          id="google-ad-button-call-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof(url) != 'undefined') {
+                window.location = url;
+              }
+            };
+            gtag('event', 'conversion', {
+                'send_to': 'AW-17610711210/4AFsCPDGtqUbEKrBuM1B',
+                'value': 1.0, 'currency': 'USD',
+                'event_callback': callback
+            });
+            return false;
+          }
+      `,
           }}
         />
       </Head>
